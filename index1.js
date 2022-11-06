dd9599aa013c80d5751ca9765ead3d9a4bf51fad
asifbinfarooq1 <115541626+asifbinfarooq1@users.noreply.github.com>
2022-11-06 20:48:56 +0500

    Add files via upload

---
+++ index1.js
@@ -0,0 +1,33 @@
+let button = document.getElementById('btn');
+button.addEventListener('click',() => {
+    const height = parseInt(getElementById('height').value);
+    const weight = parseInt(getElementById('weight').value);
+    const result = document.getElementById('output');
+    let height_status=false,weight_status=false;
+    if(height==='' || isNaN(height) (height<=0)){
+        document.getElementById('height_error').innerHTML= 'please provide a valid Height'
+    }else{
+        document.getElementById(height_error).innerHTML=''
+        height_status=true;
+    }
+
+    if(weight==='' || isNaN(weight) (weight<=0)) {
+        document.getElementById('weight_error').innerHTML= 'please provide a valid Weight'
+    }else{
+        document.getElementById('weight_error').innerHTML='';
+        weight_status=true;   
+    }
+    if(height_status && weight_status) {
+        const bmi = (weight / ((height*height)/10000)).toFixed(2);
+        if(bmi<18.6){
+            result.innerHTML='under weight:' + bmi;
+        }else if(bmi>=18.6 && bmi<24.9) {
+            result.innerHTML= 'Normal:' + bmi;
+        }else{
+            result.innerHTML='Over Weight:' +bmi
+        }
+    }else{
+        alert('The form has errors')
+        result.innerHTML='';
+    }
+});
